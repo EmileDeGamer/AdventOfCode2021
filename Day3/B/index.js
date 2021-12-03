7a305870f983eaeb3647a75733cd0f1e61702d9c
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-12-03 13:26:35 +0100

    cleaned up even more

--- Day3/B/index.js
+++ Day3/B/index.js
@@ -7,16 +7,13 @@ input.map(line => { line = line.split('')})
 let leftData = input, rightData = input
 
 for (let x = 0; x < input[0].length; x++) {
-    let arr = []
-    let arr2 = []
+    let arr = [], arr2 = []
     for (let y = 0; y < leftData.length; y++) { arr.push(leftData[y][x]) }
     for (let y = 0; y < rightData.length; y++) { arr2.push(rightData[y][x]) }
-    let oneCount = 0
-    let oneCount2 = 0
+    let oneCount = 0, oneCount2 = 0
     arr.map(number => { if(number == '1') oneCount++ })
     arr2.map(number => { if(number == '1') oneCount2++ })
-    let tLeftData = []
-    let tRightData = []
+    let tLeftData = [], tRightData = []
     let rightSplitter = oneCount2 >= Math.round(rightData.length / 2) ? "0" : "1"
     rightData.filter(line => { if(line[x] == rightSplitter) return line }).map(line =>{ tRightData.push(line) })
     let leftSplitter = oneCount >= Math.round(leftData.length / 2) ? "1" : "0"
